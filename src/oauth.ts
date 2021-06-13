2ebcf2455a4265cb3ca5fb29f71c2db2eab17fdf
Brian Yao Sheng Ng <brian-yao-sheng.ng@keysight.com>
2021-06-13 12:10:43 +0800

    Redirect url change

--- src/oauth.ts
+++ src/oauth.ts
@@ -1,6 +1,6 @@
 export const OAuthSettings = {
     appId: '3c439c13-bc8c-451d-a63c-c79ba84a70f2',
-    redirectUri: 'http://localhost:4200',
+    redirectUri: 'https://smart-interview-scheduler.herokuapp.com/',
     scopes: [
       "user.read",
       "mailboxsettings.read",
